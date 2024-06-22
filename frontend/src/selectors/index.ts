1adaec91fd74afda16169c8c50efff5952fec4d4
berson969 <102930419+berson969@users.noreply.github.com>
2024-06-22 08:08:50 +0000

    Deploying to gh-pages from @ berson969/diplom-mycloud@de559e8615b6791e1d7230f557ed7ed789b0be74 🚀

--- frontend/src/selectors/index.ts
+++
@@ -1,9 +0,0 @@
-// import { createSelector } from 'reselect';
-import { RootState } from '../store';
-
-
-export const getLoginUser = (state: RootState) => state.users.loginUser;
-export const getCurrentUser = (state: RootState) => state.users.currentUser;
-export const selectActiveState = (state: RootState) => state.users.activeState;
-export const getView = (state: RootState) => state.users.view;
-export const getEditFile = (state: RootState) => state.files.editFile;
