8fdfae375d6019ff674a1413ef24290a7c8ad48d
berson969 <berson969@gmail.com>
2024-07-01 15:19:05 +0200

    sp .env to backend

--- backend/start.sh
+++ backend/start.sh
@@ -4,7 +4,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 #flake8 .
-python manage.py test
+python manage.py test --noinput --keepdb
 
 # Запуск сервера в через gunicorn
 gunicorn -b 0.0.0.0:8000 mycloud.wsgi:application
