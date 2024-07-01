360ed75eec1d04ef8f772d40643f8a8e24f16e5a
berson969 <berson969@gmail.com>
2024-07-01 10:59:54 +0200

    change path  .env

--- backend/mycloud/settings.py
+++ backend/mycloud/settings.py
@@ -15,7 +15,9 @@ from pathlib import Path
 from corsheaders.defaults import default_headers
 from dotenv import load_dotenv
 
-load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+load_dotenv(dotenv_path)
+print(f"Loading .env from: {dotenv_path}")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
