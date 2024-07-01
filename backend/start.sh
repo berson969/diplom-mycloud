b62da7538cc6ffe0110b5399a3f6bdb7d5dd7a93
berson969 <berson969@gmail.com>
2024-07-01 08:04:57 +0200

    static

--- backend/start.sh
+++ backend/start.sh
@@ -6,8 +6,5 @@ python manage.py migrate
 flake8 .
 python manage.py test
 
-#python manage.py createsuperuser --noinput --username=admin --email=admin@example.com
-#echo "adminadmin" | python manage.py changepassword admin
-
 # Запуск сервера в через gunicorn
 gunicorn -b 0.0.0.0:8000 mycloud.wsgi:application
