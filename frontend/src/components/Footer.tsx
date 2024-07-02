7b6099a3ef072e4c176b40ed779da7e6b02426d3
berson969 <berson969@gmail.com>
2024-07-02 17:14:21 +0200

    change path logo.png

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
+	const prefix = process.env.BUILD_PREFIX || '';
 	return (
 		<div className="">
 			<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4  border-top">
 				<div className="col-md-4 d-flex align-items-center">
 					<a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
 						<img
-							src="/logo_mycloud.png"
+							src={`${prefix}logo_mycloud.png`}
 							alt="My Cloud"
 							className="logo-image-small img-fluid me-2"
 						/>
--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import "../css/logo.css"
 
 const Logo: React.FC = () => {
+	const prefix = process.env.BUILD_PREFIX || '';
+
 	return (
 		<div className="col-md-3 mb-2 mb-md-0">
-			<a href="/" className="d-inline-flex link-body-emphasis text-decoration-none my-logo">
+			<a href={`/${prefix}`}  className="d-inline-flex link-body-emphasis text-decoration-none my-logo">
 				<img
-					src="/logo_mycloud.png"
+					src={`${prefix}logo_mycloud.png`}
 					alt="My Cloud"
 					className="logo-image img-fluid me-2"
 				/>
