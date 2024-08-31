96e9d3d7e5862841db907ffa78c5c07c4832a49c
berson969 <berson969@gmail.com>
2024-08-31 13:27:46 -0400

    change cert and VITE_BASE_HOST

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
 		base: env.VITE_BUILD_PREFIX ?? '/',
 		'process.env.VITE_BASE_QUERY_URL': env.VITE_BASE_QUERY_URL,
 		'process.env.VITE_BUILD_PREFIX': env.VITE_BUILD_PREFIX
-	})
+	});
 
 	const config: UserConfig & { server?: Partial<ServerOptions> } = {
 		base: env.VITE_BUILD_PREFIX ?? '/',
@@ -23,17 +23,21 @@ export default defineConfig(({ mode }) => {
 			'process.env.VITE_BASE_QUERY_URL': JSON.stringify(env.VITE_BASE_QUERY_URL),
 			'process.env.VITE_BUILD_PREFIX': JSON.stringify(env.VITE_BUILD_PREFIX)
 		},
-	};
-
-	if (env.VITE_BASE_QUERY_URL === 'https://localhost/api') {
-		config.server = {
+		server: {
 			https: {
 				key: fs.readFileSync(path.resolve(__dirname, '../nginx/ssl/cert.key')),
 				cert: fs.readFileSync(path.resolve(__dirname, '../nginx/ssl/cert.crt')),
 			},
+		}
+	};
+
+	if (env.VITE_BASE_QUERY_URL === 'https://localhost/api') {
+		config.server = {
+			...config.server,
 			host: 'localhost',
 			port: 5173,
-		}
+		};
 	}
-	return config
-})
+		return config
+
+});
